9c8054a6b28cfcedb6e85fb52ddaf03bdafe6be1
Keitaro Shishido <keitaro.shishido@inte.co.jp>
2018-04-27 14:32:10 +0900

    frontend initial commit

---
+++ frontend/layouts/default.vue
@@ -0,0 +1,79 @@
+<template>
+<div id="app">
+  <v-app id="inspire">
+    <v-navigation-drawer
+      fixed
+      v-model="drawer"
+      app
+      :clipped="$vuetify.breakpoint.mdAndUp"
+    >
+      <v-list dense>
+        <v-list-tile @click="">
+          <v-list-tile-action>
+            <v-icon>home</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Home</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile @click="">
+          <v-list-tile-action>
+            <v-icon>contact_mail</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Contact</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar 
+      color="indigo" 
+      dark 
+      fixed 
+      app
+      :clipped-left="$vuetify.breakpoint.mdAndUp"
+    >
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-title>SPA example</v-toolbar-title>
+    </v-toolbar>
+    <v-content>
+      <v-container fluid fill-height>
+        <v-layout
+          justify-center
+          align-center
+        >
+          <v-flex text-xs-center>
+            <v-tooltip left>
+              <v-btn icon large :href="source" target="_blank" slot="activator">
+                <v-icon large>code</v-icon>
+              </v-btn>
+              <span>Source</span>
+            </v-tooltip>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-content>
+    <v-footer
+      color="indigo"
+      dark 
+      fixed 
+      app
+      :clipped-left="$vuetify.breakpoint.mdAndUp"
+    >
+      <span class="white--text">&copy; 2017</span>
+    </v-footer>
+  </v-app>
+</div>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      drawer: null
+    }),
+
+    props: {
+      source: String
+    }
+  }
+</script>
